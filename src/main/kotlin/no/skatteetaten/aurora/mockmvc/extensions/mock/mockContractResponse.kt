759b71beacd4ff173e92fef7a443a2480df48a34
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-03-20 08:39:34 +0100

    added willReturnContractResponse extension function

---
+++ src/main/kotlin/no/skatteetaten/aurora/mockmvc/extensions/mock/mockContractResponse.kt
@@ -0,0 +1,16 @@
+package no.skatteetaten.aurora.mockmvc.extensions.mock
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.mockito.BDDMockito
+
+inline fun <reified T : Any> BDDMockito.BDDMyOngoingStubbing<T?>.willReturnContractResponse(
+    name: String,
+    folder: String = "contracts",
+    objectMapper: ObjectMapper = jacksonObjectMapper()
+): BDDMockito.BDDMyOngoingStubbing<T?>? {
+    val fileName = "/$folder/$name.json"
+    val content = objectMapper.readValue<T?>(this::class.java.getResource(fileName))
+    return this.willReturn(content)
+}
